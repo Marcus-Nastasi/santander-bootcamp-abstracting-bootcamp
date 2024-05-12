4530818334a8c01e08869ebe2489a9bb50f04d0a
rolemberg <vinnie.nsts@gmail.com>
2024-05-12 11:44:04 -0300

    configuring Mentoring class;

--- src/com/santander/bootcamp/Domain/Mentoring.java
+++ src/com/santander/bootcamp/Domain/Mentoring.java
@@ -1,6 +1,41 @@
 package com.santander.bootcamp.Domain;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Mentoring {
 
+    private final String title;
+    private final String description;
+    private final Date date;
+    private final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
+    public Mentoring(String title, String description, Date date) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return(
+            "Mentoring{" + "title='" + title + '\'' +
+            ", description='" + description + '\'' +
+            ", date=" + sdf.format(date) + '}'
+        );
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
 }
+
+
