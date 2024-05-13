11b3cd04d34d3ae812f74a9243773253f8a542bd
rolemberg <vinnie.nsts@gmail.com>
2024-05-13 00:33:57 -0300

    creating Bootcamp class structure;

--- src/com/santander/bootcamp/Domain/Bootcamp.java
+++ src/com/santander/bootcamp/Domain/Bootcamp.java
@@ -1,4 +1,65 @@
 package com.santander.bootcamp.Domain;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Bootcamp {
+
+    private String name;
+    private String description;
+    private Date initDate = new Date();
+    private Date finalDate;
+    private final ArrayList<Content> contentArrayList = new ArrayList<>();
+    private final ArrayList<Dev> devArrayList = new ArrayList<>();
+    private final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Bootcamp(String name, String description, Date initDate, Date finalDate) {
+        this.name = name;
+        this.description = description;
+        this.initDate = initDate;
+        this.finalDate = finalDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getInitDate() {
+        return initDate;
+    }
+
+    public void setInitDate(Date initDate) {
+        this.initDate = initDate;
+    }
+
+    public Date getFinalDate() {
+        return finalDate;
+    }
+
+    public void setFinalDate(Date finalDate) {
+        this.finalDate = finalDate;
+    }
+
+    public ArrayList<Content> getContentArrayList() {
+        return contentArrayList;
+    }
+
+    public ArrayList<Dev> getDevArrayList() {
+        return devArrayList;
+    }
 }
+
+
